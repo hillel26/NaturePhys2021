642d58c8d14770b4fed7c055fa0a8fd328325f3e
hillel26 <63084907+hillel26@users.noreply.github.com>
2021-11-16 16:27:25 +0200

    Delete code_for_web directory

--- code_for_web/code/functions/find_x_via_omega.m
+++
@@ -1,5 +0,0 @@
-function Xss = find_x_via_omega(omega,M,all_or_eff,toplot,conditions,release)
-global A Anw x0 Factor_Xeff
-A = omega*Anw;
-Factor_Xeff = sum(A,1)/sum(sum(A));
-Xss = SolveOdes(x0,M,all_or_eff, toplot,conditions,release);
--- code_for_web/code/functions/is_rcoverable_theory.m
+++
@@ -1,8 +0,0 @@
-function is_it = is_rcoverable_theory(w,k,d,range_sols)
-    % determine if the dynamics is recoverable based on theory of
-    % intersection between functions
-    % d is the value of exciting, range_sols is the range in which all
-    % states should be (x_high, x_low)
-    dc = find_dc_by_wk_theory_intersection(w,k,range_sols);
-    is_it = d>=dc;       
-end
