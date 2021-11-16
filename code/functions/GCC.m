9266d0a1e3c8e1798e898dd6795a5899a3353a19
hillel26 <63084907+hillel26@users.noreply.github.com>
2021-11-16 17:01:06 +0200

    Delete code directory

--- code/functions/GCC.m
+++
@@ -1,8 +0,0 @@
-function gcc = GCC(A)
-%ONLYGCC Summary of this function goes here
-%   Detailed explanation goes here
-[cb, sizes] = conncomp(graph(A));
-[~,indGcc] = max(sizes);
-gcc = find(cb==indGcc);
-end
-
